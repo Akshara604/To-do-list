6b85096a11631c55b62dc285e0a60bfb12575b9d
Akshara S PA <74604911+Akshara604@users.noreply.github.com>
2022-01-05 19:01:41 +0530

    Connecting mongoose

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 //use let instead of var as var can be accessed as a global variable
 // const arrays can still be used to push items
-const items = ["Exercise", "Code", "Sleep"];
-const workItems = [];
+// const items = ["Exercise", "Code", "Sleep"];
+// const workItems = [];
 
 const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model(
+  "Item",
+  itemsSchema
+);
+
+const item1 = new Item({
+  name: "Do yoga",
+});
+
+const item2 = new Item({
+  name: "Eat Breakfast",
+});
+
+const item3 = new Item({
+  name: "Code",
+});
+
+const defaultItems = [item1,item2,item3];
+
+Item.insertMany(defaultItems,function(err){
+  if(err){
+    console.log("error");
+  } else{
+    console.log("Sucsess");
+  }
+});
+
 app.get("/", function (req, res) {
   const day = date.getDate();
-  res.render("lists", { listTitle: day, newListItems: items });
+  res.render("lists", { listTitle: "Today", newListItems: items });
 });
 app.post("/", function (req, res) {
   const item = req.body.newItem;
